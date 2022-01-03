7141514f63befbb614726e3d4827b5f9d7287b33
Udo-Eng <udochukwuabazie.soft@gmail.com>
2022-01-03 12:39:53 +0100

    Finally finished the Backend Code and DataBase

--- Server.js
+++ Server.js
@@ -1,136 +1,45 @@
 const express = require('express');
-// const BodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const app = express();
 const cors = require('cors');
+const { getUsers, insertUser, ValidateUserLogin, getUserById, upDateUserEntries } = require('./db.js');
 
+
+
+
+//Adding the cors middleware to handle cross origin resource sharing between front end and backend 
 app.use(cors());
 //Adding an express middleware to handle json 
 app.use(express.json());
 
-//Adding the cors middleware to handle cross origin resource sharing between front end and backend  
-
-
-//Creating a database object to mimick a database
-const database = {
-    users: [
-        {
-            id: '12',
-            name: 'abazie udo ',
-            email: 'udo@gmail.com',
-            password: '123',
-            entries: 0,
-            joined: new Date(),
-        },
-        {
-            id: '0390',
-            name: 'gold bee ',
-            email: 'bee@gmail.com',
-            password: 'food',
-            entries: 0,
-            joined: new Date(),
-        },
-    ],
-    login: [
-        {
-            id: '',
-            hash: '',
-            email: 'udo@gmail.com'
-        }
-    ]
-}
-
-
-
-
-//Main  end point for this application 
-app.get('/', (req, res) => {
-    res.send(database.users)
-})
-
-// SignIn end point 
-app.post('/signin', (req, res) => {
-    // res.send('signin successfully thanks for using this application')
-
-    if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
-
-        res.json('success')
-    } else {
-        console.log('error logging into the server ')
-        res.status(400).json('error logging in')
-    }
-})
-
-//Creating the register end point 
-app.post('/register', (req, res) => {
-
-    const { name, email, password } = req.body;
-
-    const generateId = () => {
-        let range = 10000000;
-        return Math.floor(Math.random() * range + 3768787);
-    }
-    //encrypting the password using bcrypt 
-    bcrypt.hash(password, null, null, (err, res) => {
-        ;
-    })
-
-    const newUser = {
-        id: '1234',
-        name: name,
-        email: email,
-        password: hash,
-        entries: 0,
-        joined: new Date(),
-    }
-
-    database.users.push(newUser);
-    res.json(database.users[database.users.length - 1]);
-})
-
-
-//Creating and end point to get user profile 
-app.get('/profile/:userId', (req, res) => {
-
-    const { userId } = req.params;
-    found = false;
-
-    database.users.forEach(user => {
-        if (user.id === userId) {
-            found = true;
-            res.json(user);
-        }
-    });
-    if (!found) {
-        res.status(404).json('user not found ')
-    }
-})
-
-//Creating and end point to get user profile 
-app.put('/image', (req, res) => {
-
-    const { id } = req.body;
-    found = false;
-
-    database.users.forEach(user => {
-        if (user.id === id) {
-
-            found = true
-            user.entries++;
-            res.json(user.entries);
-        }
-    });
-    if (!found) {
-        res.status(404).json('user not found ')
-    }
-})
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-})
-
+// //Creating a database object to mimick a database
+// const database = {
+//     users: [
+//         {
+//             id: '12',
+//             name: 'abazie udo ',
+//             email: 'udo@gmail.com',
+//             password: '123',
+//             entries: 0,
+//             joined: new Date(),
+//         },
+//         {
+//             id: '0390',
+//             name: 'gold bee ',
+//             email: 'bee@gmail.com',
+//             password: 'food',
+//             entries: 0,
+//             joined: new Date(),
+//         },
+//     ],
+//     login: [
+//         {
+//             id: '',
+//             hash: '',
+//             email: 'udo@gmail.com'
+//         }
+//     ]
+// }
 // Before implementing any API you must plan the API 
 /*
 / --> res == this is working
@@ -139,3 +48,113 @@ app.listen(PORT, () => {
 /profile/:userId --> GET = user
 /image --> PUT = user
 */
+
+
+//Main  end point for this application 
+app.get('/', async (req, res) => {
+    let users = await getUsers();
+    res.json(users);
+})
+
+// SignIn end point 
+app.post('/signin', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        console.log('req was received sucessfully');
+        await ValidateUserLogin(email, password, res);
+
+    } catch (err) {
+        res.status(400).json('Server Error please try again Later thanks ');
+    }
+
+    // if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
+    //     res.json('success')
+    // } else {
+    //     console.log('error logging into the server ');
+    //     res.status(400).json('error logging in')
+    // }
+})
+
+//Creating the register end point 
+app.post('/register', async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+
+        //encrypting the password using bcrypt 
+        bcrypt.hash(password, null, null, async (err, hash) => {
+            if (err) console.log(err);
+
+            //Creating a new Userwith hashed password
+            const newUser = {
+                name: name,
+                email: email,
+                entries: 0,
+                joined: new Date(),
+                hash: hash,
+            }
+            await insertUser(newUser, res);
+        })
+
+    } catch (err) {
+        res.status(400).json('error registering user')
+    }
+})
+
+
+//Creating and end point to get user profile 
+app.get('/profile/:userId', async (req, res) => {
+    try {
+        const { userId } = req.params;
+
+        let user = await getUserById(userId);
+
+        if (user.length) {
+            res.json(user[0]);
+        } else {
+            res.status(400).json('user not found');
+        }
+
+    } catch (err) {
+        res.status(400).json('error finding user');
+    }
+
+})
+
+//Creating and end point to get user profile 
+app.put('/image', async (req, res) => {
+    try {
+        const { id } = req.body;
+
+        const entries = await upDateUserEntries(id);
+
+        if (entries.length) {
+            res.json(entries[0]);
+        } else {
+            res.status(400).json('Unabale to get user entries');
+        }
+    } catch (err) {
+        res.status(400).json('Server error unable to update user entries');
+    }
+
+    // Redundant code no longer in use 
+    // found = false;
+
+    // database.users.forEach(user => {
+    //     if (user.id === id) {
+
+    //         found = true
+    //         user.entries++;
+    //         res.json(user.entries);
+    //     }
+    // });
+    // if (!found) {
+    //     res.status(404).json('user not found ')
+    // }
+})
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
+
---
+++ db.js
@@ -0,0 +1,109 @@
+/*
+Get Users
+insert Users
+Validate  Users
+update Users information
+*/
+const bcrypt = require('bcrypt-nodejs');
+const knex = require('knex');
+const { credentials } = require('../config');
+
+//Configuring the Database 
+const postgres = knex({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        port: 5432,
+        user: 'postgres',
+        password: credentials.password,
+        database: 'postgres'
+    }
+});
+
+
+exports.getUsers = async () => {
+    const users = await postgres.select('*').from('users');
+    return users;
+}
+
+exports.insertUser = async (newUser, res) => {
+
+    let { name, email, entries, joined, hash } = newUser;
+
+
+
+    postgres.transaction(async trx => {
+        try {
+            const RegisterEmail = await trx
+                .insert({
+                    email,
+                    hash
+                }).into('login')
+                .returning('email');
+
+            let User = {
+                name,
+                email: RegisterEmail[0],
+                entries,
+                hash,
+                joined
+            }
+
+            let results = await trx('users').returning('*').insert(User);
+
+            await trx.commit;
+            if (results.length) {
+                // response with the user that registered
+                res.json(results[0])
+            }
+        } catch (err) {
+            if (err) {
+                await trx.rollback;
+                res.status(400).json('user not sucessfully registered');
+            }
+        }
+    })
+
+}
+
+//query the database To validate Login  info
+exports.ValidateUserLogin = async (email, password, res) => {
+    try {
+        const results = await postgres.select('hash').from('login').where('email', '=', email);
+
+        if (!results.length) res.status(400).json('user login details is invalid');
+
+        let hash = results[0].hash;
+
+        bcrypt.compare(password, hash, async (err, result) => {
+            if (err) {
+                console.log(err);
+                res.status(400).json('user login details is invalid');
+            }
+            if (result) {
+                const user = await postgres.select('*').from('users').where('email', '=', email);
+                res.json(user[0]);
+            } else {
+                console.log('req was not sent sucessfully');
+                res.status(400).json('Unable to get user Please register again ');
+            }
+        })
+    } catch (err) {
+        res.status(400).json('Server Error Please try again later thanks');
+    }
+
+
+}
+
+exports.getUserById = async (userId) => {
+    const user = await postgres.select('*').from('users').where({ id: userId });
+
+    return user;
+}
+
+exports.upDateUserEntries = async (userId) => {
+    const entries = await postgres('users').where('id', '=', userId).increment('entries', 1).returning('entries');
+
+
+    return entries;
+}
