d471abab957871c47684b3628ac4e0b232da8a90
Udo-Eng <udochukwuabazie.soft@gmail.com>
2022-01-04 16:13:11 +0100

    detecting bug

--- controllers/register.js
+++ controllers/register.js
@@ -2,30 +2,30 @@
 const { insertUser } = require('../db.js');
 
 exports.register = async (req, res) => {
-    try {
-        const { name, email, password } = req.body;
+    // try {
+    const { name, email, password } = req.body;
 
-        if (!email || !password || !name) {
-            return res.status(400).json('Please enter the right details')
+    if (!email || !password || !name) {
+        return res.status(400).json('Please enter the right details')
 
-        }
-        //encrypting the password using bcrypt 
-        bcrypt.hash(password, null, null, async (err, hash) => {
-            if (err) console.log(err);
-
-            //Creating a new Userwith hashed password
-            const newUser = {
-                name: name,
-                email: email,
-                entries: 0,
-                joined: new Date(),
-                hash: hash,
-            }
-            await insertUser(newUser, res);
-        })
-
-    } catch (err) {
-        console.log(err);
-        res.status(400).json('error registering user')
     }
+    //encrypting the password using bcrypt 
+    bcrypt.hash(password, null, null, async (err, hash) => {
+        if (err) console.log(err);
+
+        //Creating a new Userwith hashed password
+        const newUser = {
+            name: name,
+            email: email,
+            entries: 0,
+            joined: new Date(),
+            hash: hash,
+        }
+        await insertUser(newUser, res);
+    })
+
+    // } catch (err) {
+    //     console.log(err);
+    //     res.status(400).json('error registering user')
+    // }
 }
