e23d7073a08520b8a1b96f3f7c3e230bf30a41d1
Udo-Eng <udochukwuabazie.soft@gmail.com>
2022-01-04 12:31:35 +0100

    Revert "modified getUsers route to check heroku"
    This reverts commit 73fe3b81be3500f58472380f142dc9b99f00c755.
    
    reverting the getUsers file

--- controllers/getUsers.js
+++ controllers/getUsers.js
@@ -1,11 +1,6 @@
 const { getUsers } = require('../db.js');
 
 exports.GetUsers = async (req, res) => {
-    // let users = await getUsers();
-    let users = {
-        name: 'Udo',
-        email: 'udo@macbulk.com',
-        entries: 10
-    }
+    let users = await getUsers();
     res.json(users);
 }
