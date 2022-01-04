73fe3b81be3500f58472380f142dc9b99f00c755
Udo-Eng <udochukwuabazie.soft@gmail.com>
2022-01-04 12:07:30 +0100

    modified getUsers route to check heroku

--- controllers/getUsers.js
+++ controllers/getUsers.js
@@ -1,6 +1,11 @@
 const { getUsers } = require('../db.js');
 
 exports.GetUsers = async (req, res) => {
-    let users = await getUsers();
+    // let users = await getUsers();
+    let users = {
+        name: 'Udo',
+        email: 'udo@macbulk.com',
+        entries: 10
+    }
     res.json(users);
 }
